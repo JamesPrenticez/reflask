738d493a1697d5dd4671b7cfe8c6c3d1de9be4f4
James Prentice <jamesprenticez@gmail.com>
2022-09-19 15:08:11 +1200

    First deploy

--- backend/errors/routes.py
+++ backend/errors/routes.py
@@ -3,7 +3,7 @@ from flask import Blueprint, render_template, redirect, url_for
 blueprint = Blueprint('errors', __name__)
 
 @blueprint.app_errorhandler(404)
-def not_found_error():
+def not_found_error(error):
     return redirect(url_for('errors.not_found'))
 
 @blueprint.route('/error/404', methods=['GET'])
@@ -11,7 +11,7 @@ def not_found():
     return render_template('index.html'), 404
 
 @blueprint.app_errorhandler(500)
-def internal_server_error():
+def internal_server_error(error):
     return redirect('/errors.internal_server')
 
 @blueprint.route('/error/500', methods=['GET'])
